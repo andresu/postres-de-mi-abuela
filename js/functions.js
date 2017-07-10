1a9eef018952b949077a8a9a6d4e95391deed153
Andres Urrea <andres.urreao@gmail.com>
2017-07-09 21:06:46 -0500

    Primer import

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+$('#mobile-menu').click(function() {
+	$('.left').toggleClass('open');
+	$('.right').toggleClass('move');
+});
+
+/*$('.grid').masonry({
+  itemSelector: '.grid-item',
+  columnWidth: '.grid-sizer',
+  percentPosition: true,
+});*/
