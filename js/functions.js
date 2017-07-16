0874cc4e5884183912b0f57577ab5011105e6cbd
Andres Urrea <andres.urreao@gmail.com>
2017-07-15 23:07:24 -0500

    post page

--- js/functions.js
+++ js/functions.js
@@ -1,12 +1,8 @@
 $('#mobile-menu').click(function() {
-	$('.left').toggleClass('open');
-	$('.right').toggleClass('move');
+	$('.wrapper').toggleClass('open');
 });
 
-/*$('.grid').masonry({
-  itemSelector: '.grid-item',
-  columnWidth: '.grid-sizer',
-  percentPosition: true,
-});*/
+
+
 
 
